addba26be591c906bf85c6a35da9fe2d1edbed6e
areebn321 <areebnadir3@gmail.com>
2026-03-28 19:03:55 +0500

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -8,7 +8,7 @@ const getTransporter = async () => {
   transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,          // smtp.gmail.com
     port: Number(process.env.EMAIL_PORT),  // 587
-    secure: false,                         // ⚠️ must be false
+    secure: process.env.EMAIL_SECURE === 'true',                         // ⚠️ must be false
     auth: {
       user: process.env.EMAIL_USER,        // your gmail
       pass: process.env.EMAIL_PASS,        // app password
